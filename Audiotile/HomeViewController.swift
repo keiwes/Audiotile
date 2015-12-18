968a5c5f9337c72a1fd81988eda7776914cd0b12
Zarek Parker <pair+zparker@pivotallabs.com>
2015-12-17 23:57:03 -0500

    WIP - figuring out testing frameworks

--- Audiotile/HomeViewController.swift
+++ Audiotile/HomeViewController.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    
+    @IBAction func addButtonTapped(sender: AnyObject) {
+        let alert2 = UIAlertView(title: "Add", message: "You hit the add button", delegate: nil, cancelButtonTitle: "Cancel", otherButtonTitles: "Cool Bro", "Shut up")
+        alert2.show()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
